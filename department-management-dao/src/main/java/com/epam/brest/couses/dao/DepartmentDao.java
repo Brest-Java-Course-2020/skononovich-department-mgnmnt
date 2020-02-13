582d33f1628a6d756e934c4193e6482abf15c14a
sergeykon <beergood6@gmail.com>
2020-02-14 01:28:43 +0300

    create dao and model layers. Add maven builder

---
+++ department-management-dao/src/main/java/com/epam/brest/couses/dao/DepartmentDao.java
@@ -0,0 +1,11 @@
+package com.epam.brest.couses.dao;
+
+
+import com.epam.brest.courses.model.Department;
+
+import java.util.List;
+
+public class DepartmentDao {
+
+    public List<Department> getDepartments;
+}
