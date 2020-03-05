09955f0cad18fc77e2a66960200e4b6372d583ec
sergeykon <beergood6@gmail.com>
2020-03-06 02:06:51 +0300

    Add DepartmentDtoDaoJdbcImpl and test

---
+++ department-management-dao/src/main/java/com/epam/brest/courses/dao/DepartmentDtoDaoJdbcImpl.java
@@ -0,0 +1,33 @@
+package com.epam.brest.courses.dao;
+
+import com.epam.brest.courses.model.dto.DepartmentDto;
+
+import com.epam.brest.courses.model.dto.DepartmentDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class DepartmentDtoDaoJdbcImpl implements DepartmentDtoDao {
+
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    @Value("${departmentDto.findAllWithAvgSalary}")
+    private String findAllWithAvgSalarySql;
+
+
+    public DepartmentDtoDaoJdbcImpl(NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+    }
+
+    @Override
+    public List<DepartmentDto> findAllWithAvgSalary() {
+        List<DepartmentDto> departments = namedParameterJdbcTemplate.query(findAllWithAvgSalarySql,
+                BeanPropertyRowMapper.newInstance(DepartmentDto.class));
+        return departments;
+    }
+
+}
